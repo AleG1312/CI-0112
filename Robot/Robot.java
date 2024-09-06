b986a09ed74f61d85167049dd571087010c55dd9
Alejandro Guerra Rodriguez <C13451@ecci.ucr.ac.cr>
2024-09-06 16:36:51 -0600

    Primer commit del Laboratorio 2 (El paso de las instrucciones)

---
+++ Robot/Robot.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a description of class Robot here.
+ *
+ * @author Alejandro Guerra Rodríguez
+ * @version 12-09-2024
+ */
+public class Robot
+{
+    //Defino los atributos
+    private String nombre;      //El nombre del robot
+    private int puntosVida;     //Cantidad de vida que tiene (entre 50 y 100)
+    private int ataque;         //Cantidad de daño que causa (entre 10 y 20)
+    
+    //Defino el método constructor
+    
+    
+    //Defino los setters y los getters
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    
+    
+    public void setPuntosVida(int puntosVida){
+        this.puntosVida = puntosVida;
+    }
+    public int getPuntosVida(){
+        return this.puntosVida ;
+    }
+    
+    public void Set(){
+        this. = ;
+    }
+    public get(){
+        return this. ;
+    }
+    
+    public void Set(){
+        this. = ;
+    }
+    public get(){
+        return this. ;
+    }
+    
+}
